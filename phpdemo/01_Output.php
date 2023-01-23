a227f7631b8bebb6386f558002ac0badb6d759ab
neillll04 <nelsumalde04@gmail.com>
2023-01-23 10:43:23 +0800

    my first php

---
+++ phpdemo/01_Output.php
@@ -0,0 +1,46 @@
+<?php
+/* ----- Variables & Data Types ----- */
+
+/* --------- PHP Data Types --------- */
+/*
+- String - A string is a series of characters surrounded by quotes
+- Integer - Whole numbers
+- Float - Decimal numbers
+- Boolean - true or false
+- Array - An array is a special variable, which can hold more than one value
+- Object - A class
+- NULL - Empty variable
+- Resource - A special variable that holds a resource
+*/
+
+/* --------- Variable Rules --------- */
+/*
+- Variables must be prefixed with $
+- Variables must start with a letter or the underscore character
+- variables can't start with a number
+- Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+- Variables are case-sensitive ($name and $NAME are two different variables)
+*/
+
+// echo "Hello ryan";
+// echo "<br>"; 
+// echo 123;
+
+// echo '<h1>Hello</h1>';
+
+//print is similar tp echo, but a bit slower
+// print 'Hello';
+
+//print_r - gives a bit more info can be used to print arrays
+// print_r('Hello');
+// echo "<br>";
+// print_r([1,2,3]);
+
+//var_dump - more info data type and length
+//var_dump('hello');
+// var_dump([1,2,3]);
+
+// $fname = 'Neil';
+// $age = 41; 
+
+// echo "$fname is $age years old mainggit kayo";
